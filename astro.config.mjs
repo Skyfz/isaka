c448bea064f5fdb4c878dc0dcf627d0fe5174eb0
Skyfz <mikesap34@gmail.com>
2024-03-22 20:34:33 +0200

    2nd commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,8 +25,8 @@ const whenExternalScripts = (items = []) =>
     : [];
 
 export default defineConfig({
-  site: SITE.site,
-  base: SITE.base,
+  site: 'https://skyfz.github.io',
+  //base: '/isaka',
   trailingSlash: SITE.trailingSlash ? 'always' : 'never',
 
   output: 'static',
