c68a93786e503fcd43cd84ff733b00978f64e76b
Skyfz <mikesap34@gmail.com>
2024-03-23 09:15:32 +0200

    Your commit message

---
+++ push_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Add all changes
+git add -A
+
+# Commit with a specified message
+git commit -m "Your commit message"
+
+# Push changes to the 'main' branch on the remote named 'origin'
+git push origin main
