e9a31a62488ec551aae6ed09b7847494b0d35e0a
Skyfz <mikesap34@gmail.com>
2024-04-30 02:36:20 +0200

    Commit on 2024-04-30 02:36:20

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ const whenExternalScripts = (items = []) =>
 
 export default defineConfig({
   site: 'https://isakaconsult.co.za',
-  base: '/isaka',
+  //base: '/isaka',
   trailingSlash: SITE.trailingSlash ? 'always' : 'never',
 
   output: 'static',
